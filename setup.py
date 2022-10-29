967938efedc835d02f87001a5a4054a3a12d1b85
XUDONGYU <xudongyu@bupt.edu.cn>
2022-10-30 02:06:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
         version="1.0.0",
         author="Dongyu Xu",
         author_email="xudongyu@bupt.edu.cn",
-        description="a filter to remove the still object",
+        description="a filter to remove the stationary object",
         packages = find_packages(
             exclude = ['test*']
         )
